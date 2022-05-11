bce1096e447bdf67d2b1753aa468b610906791d5
Maciej Czepan <maciej.czepan@gmail.com>
2022-05-11 12:29:24 +0200

    initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	return (
+		<div>
+			{robots.map((robot) => (
+				<Card
+					key={robot.id}
+					id={robot.id}
+					name={robot.name}
+					username={robot.username}
+					email={robot.email}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default CardList;
---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: '',
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ robots: users }));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	};
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots?.filter((r) =>
+			r.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+		);
+		return robots?.length ? (
+			<div className="tc">
+				<h1 className="f1">RoboFriends</h1>
+				<SearchBox
+					searchfield={searchfield}
+					searchChange={this.onSearchChange}
+				/>
+				<Scroll>
+					<CardList robots={filteredRobots} />
+				</Scroll>
+			</div>
+		) : (
+			<h1>Loading</h1>
+		);
+	}
+}
+
+export default App;
